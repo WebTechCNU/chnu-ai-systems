8ee09ee7d0110913e50fce58b34ff7d787ccf957
Vasyl Melnyk <windchange7@gmail.com>
2026-01-15 19:23:31 +0200

    upd

---
+++ src/app.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+app = FastAPI()
+
+# CORS:
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://webtechcnu.github.io", "http://127.0.0.1:5500", "http://127.0.0.1:5501"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.post("/api/math-faculty")
+async def math_faculty(data: dict):
+    print("Received data:", data)
+    return {"status": "success", "data_received": data}
+
+@app.post("/api/locations")
+async def locations(data: dict):
+    print("Received data:", data)
+    return {"status": "success", "data_received": data}
+
+@app.post("/api/qa")
+async def qa(data: dict):
+    print("Received data:", data)
+    return {"status": "success", "data_received": data}
+
+@app.post("/api/romanian-culture")
+async def romanian_culture(data: dict):
+    print("Received data:", data)
+    return {"status": "success", "data_received": data}
